06c7c679e298607d11e2a2f6a17c104adbe3432d
unknown <106647536+SkullCuong@users.noreply.github.com>
2023-03-20 20:34:53 +0700

    Chart

--- Controllers/IdeaController.cs
+++ Controllers/IdeaController.cs
@@ -16,11 +16,13 @@ namespace IdeaWeb.Controllers
         private IWebHostEnvironment _hostEnvironment;
         private readonly IdeaWebContext _context;
         private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IdeaWebContext _secondContext;
         public IdeaController(IdeaWebContext context, IWebHostEnvironment hostEnvironment, IHttpContextAccessor httpContextAccessor)
         {
             _context = context;
             _hostEnvironment = hostEnvironment;
             _httpContextAccessor = httpContextAccessor;
+            _secondContext = context;
         }
 
         // GET: Idea
@@ -154,6 +156,68 @@ namespace IdeaWeb.Controllers
             ViewData["UserId"] = new SelectList(_context.User, "id", "id", idea.UserId);
             return View(idea);
         }
+        public async Task<IActionResult> UserEditIdea(int? id)
+        {
+
+
+            var idea = await _context.Idea.FindAsync(id);
+            if (idea == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Category, "Id", "Name", idea.CategoryId);
+            return View(idea);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> UserEditIdea(IFormFile image, IFormFile document, [Bind("Id,Name,Content,CategoryId")] Idea Editidea)
+        {
+            var idea = await _secondContext.Idea.FindAsync(Editidea.Id);
+            string wwwRootPath = _hostEnvironment.WebRootPath;
+            try
+            {
+                idea.Name = Editidea.Name != null ? Editidea.Name : idea.Name;
+                idea.Content = Editidea.Content != null ? Editidea.Content : idea.Content;
+                idea.CategoryId = Editidea.CategoryId != null ? Editidea.CategoryId : idea.CategoryId;
+                if (image != null)
+                {
+                    string fileName = Path.GetFileNameWithoutExtension(image.FileName);
+                    string extension = Path.GetExtension(image.FileName);
+                    string image_Path = Path.Combine(wwwRootPath + "/Image/", fileName + extension);
+                    using (var fileStream = new FileStream(image_Path, FileMode.Create))
+                    {
+                        await image.CopyToAsync(fileStream);
+                    }
+                    idea.Image = fileName + extension;
+                }
+                if (document != null)
+                {
+                    string documentName = Path.GetFileName(document.FileName);
+                    string document_Path = Path.Combine(wwwRootPath + "/Document/", documentName);
+                    using (var fileStream = new FileStream(document_Path, FileMode.Create))
+                    {
+                        await document.CopyToAsync(fileStream);
+                    }
+                    idea.File = documentName;
+
+                }
+                _context.Update(idea);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!IdeaExists(Editidea.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction("UserViewIdea", "Idea", new { id = idea.Id });
+        }
+
 
         // GET: Idea/Delete/5
         public async Task<IActionResult> Delete(int? id)
@@ -186,7 +250,13 @@ namespace IdeaWeb.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-
+        public async Task<IActionResult> UserDelete(int? id)
+        {
+            var idea = await _context.Idea.FindAsync(id);
+            _context.Idea.Remove(idea);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("IdeaIndex", "Idea");
+        }
         private bool IdeaExists(int id)
         {
             return _context.Idea.Any(e => e.Id == id);
@@ -247,6 +317,7 @@ namespace IdeaWeb.Controllers
             ViewBag.comment = _context.Comment.Include(c => c.user).Where(c => c.ideaId == id).OrderByDescending(p => p.Date_Upload);
             ViewBag.commentCount = _context.Comment.Where(c => c.ideaId == id).Count();
             ViewBag.id = id;
+            ViewBag.UserId = HttpContext.Session.GetInt32("_ID").GetValueOrDefault();
             return View(idea);
         }
         public async Task<IActionResult> IdeaIndex()
@@ -258,6 +329,7 @@ namespace IdeaWeb.Controllers
 
         public FileResult DocumentDownload(int id)
         {
+
             string wwwRootPath = _hostEnvironment.WebRootPath;
             var idea = _context.Idea.FirstOrDefault(e => e.Id == id);
             var file = wwwRootPath + "/Document/" + idea.File;
