05678a0bfda580fc5d27ea5639d1526a9b0cb487
hnmluan <121085924+hnmluan@users.noreply.github.com>
2023-03-29 15:10:50 +0700

    Update UI

--- Models/User.cs
+++ Models/User.cs
@@ -4,26 +4,35 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace IdeaWeb.Models;
 public class User
 {
-
+    [Display(Name = "ID")]
     public int id { get; set; }
+
     [Required]
+    [Display(Name = "Name")]
     public string? name { get; set; }
+
     [Required]
+    [Display(Name = "Phone")]
     public string? phone { get; set; }
 
     [DataType(DataType.Date)]
+    [Display(Name = "DOB")]
     public DateTime? dob { get; set; }
     [Required]
+    [Display(Name = "Email")]
     public string? email { get; set; }
     [Required]
+    [Display(Name = "Password")]
     public string? password { get; set; }
     [Required]
+    [Display(Name = "Flag")]
     public int flag { get; set; } = 0;
     [Required]
+    [Display(Name = "Department ID")]
     public int DepartmentId { get; set; }
-
+    [Display(Name = "Department")]
     public Department? Department { get; set; }
-
+    [Display(Name = "User Role")]
     public ICollection<UserRole>? userRoles { get; set; }
 
     public ICollection<Rating>? ratings { get; set; }
